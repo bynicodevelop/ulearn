9224ab0e5c215c242e70e7ecd5f9b373ec36cec4
nico-develop <contact@nico-develop.fr>
2020-09-26 12:07:32 +0200

    issue #8 - Intégration de l'internationnalisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:leadee/screens/loading.dart';
 import 'package:leadee/share/palette.dart';
 
-void main() => runApp(App());
+void main() async {
+  final FlutterI18nDelegate flutterI18nDelegate = FlutterI18nDelegate(
+    translationLoader: FileTranslationLoader(
+        useCountryCode: false,
+        fallbackFile: 'en',
+        basePath: 'assets/i18n',
+        forcedLocale: Locale('en')),
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await flutterI18nDelegate.load(null);
+
+  runApp(App(flutterI18nDelegate));
+}
 
 class App extends StatelessWidget {
+  final FlutterI18nDelegate flutterI18nDelegate;
+
+  App(this.flutterI18nDelegate);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
-          primaryColor: Palette.blue[100]),
-      home: LoadingComponent(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
+            primaryColor: Palette.blue[100]),
+        home: LoadingComponent(),
+        localizationsDelegates: [
+          flutterI18nDelegate,
+        ],
+        builder: FlutterI18n.rootAppBuilder());
   }
 }
