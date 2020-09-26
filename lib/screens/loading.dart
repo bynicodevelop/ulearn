2ab3a6f41696fc9c70bc335106b41c83126fa567
nico-develop <contact@nico-develop.fr>
2020-09-26 11:37:10 +0200

    Fixe #3 - Ajout du widget SingleChildScrollView pour être cohérent avec les autres vues.

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -14,40 +14,43 @@ class _LoadingComponentState extends State<LoadingComponent> {
     return SafeArea(
       child: Scaffold(
         body: Center(
-          child: Container(
-            alignment: AlignmentDirectional(0.0, 0.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
             child: Container(
-              constraints: BoxConstraints(
-                  maxHeight: 100.0,
-                  maxWidth: 233.0,
-                  minWidth: 150.0,
-                  minHeight: 50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  RichText(
-                      text: TextSpan(
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 50,
-                              fontWeight: FontWeight.bold,
-                              shadows: [
-                                Shadow(
-                                    color: Colors.black.withOpacity(.5),
-                                    offset: Offset(0, 1),
-                                    blurRadius: 2)
-                              ]),
-                          text: 'lead'.toUpperCase(),
-                          children: [
-                        TextSpan(
-                            text: 'ee'.toUpperCase(),
-                            style: TextStyle(color: Palette.orange[50]))
-                      ])),
-                  Text(
-                    'loading...',
-                    style: TextStyle(letterSpacing: 2),
-                  )
-                ],
+              alignment: AlignmentDirectional(0.0, 0.0),
+              child: Container(
+                constraints: BoxConstraints(
+                    maxHeight: 100.0,
+                    maxWidth: 233.0,
+                    minWidth: 150.0,
+                    minHeight: 50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    RichText(
+                        text: TextSpan(
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
+                                      color: Colors.black.withOpacity(.5),
+                                      offset: Offset(0, 1),
+                                      blurRadius: 2)
+                                ]),
+                            text: 'lead'.toUpperCase(),
+                            children: [
+                          TextSpan(
+                              text: 'ee'.toUpperCase(),
+                              style: TextStyle(color: Palette.orange[50]))
+                        ])),
+                    Text(
+                      'loading...',
+                      style: TextStyle(letterSpacing: 2),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
